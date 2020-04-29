c9678fe44bdcc6d4efba9f19d8c7c8967a3c5322
King <1309535524@qq.com>
2020-04-29 16:48:46 +0800

    删除应用密钥

--- reader-admin/src/main/java/com/book/store/common/CommonConstent.java
+++ reader-admin/src/main/java/com/book/store/common/CommonConstent.java
@@ -1,8 +1,6 @@
 package com.book.store.common;
 
 public class CommonConstent {
+	public static String WX_BASE_URL = "https://api.weixin.qq.com/";
 	
-	public static String APPID = "wxe1674daeae57c4d8";
-
-	public static String APP_SECRET = "faac7b6877156f5277cd6e59256103b3";
 }
